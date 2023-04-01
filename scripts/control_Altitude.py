35803c9824f1aaeb0d234dd68b7e510431643357
Xpsp <juliodltv@gmail.com>
2023-04-01 16:13:06 -0600

    add pub --once takeoff and land fix landing mode

--- scripts/control_Altitude.py
+++ scripts/control_Altitude.py
@@ -6,22 +6,49 @@ from bebop_msgs.msg import Ardrone3PilotingStateAltitudeChanged
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
 
+# rostopic pub --once equivalent on node
+# https://get-help.robotigniteacademy.com/t/how-to-publish-once-only-one-message-into-a-topic-and-get-it-to-work/346
+
+# modes = 'altitude', 'landing', ...
+
 class ControlAltitude():
     def __init__(self):
-        self.velocity_pub = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size=10)
-        self.takeoff_pub = rospy.Publisher('/bebop/takeoff', Empty, queue_size=10)
-        self.land_pub = rospy.Publisher('/bebop/land', Empty, queue_size=10)
+        
+        # Publishers and Subscribers
+        self.cmd_vel_pub_ = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size=10)
+        self.takeoff_pub_ = rospy.Publisher('/bebop/takeoff', Empty, queue_size=10)
+        self.land_pub_ = rospy.Publisher('/bebop/land', Empty, queue_size=10)
         rospy.Subscriber("/bebop/states/ardrone3/PilotingState/AltitudeChanged", Ardrone3PilotingStateAltitudeChanged, self.alt_callback, queue_size=10)
         
-        msg = Empty()
-        self.takeoff_pub.publish(msg)
+        # Msgs
+        self.cmd_vel_msg = Twist()
+        self.takeoff_msg = Empty()
+        self.land_msg = Empty()
         
-        self.goal = 0
+        # Variables
+        self.minimal_altitude = 0.4
+        self.goal = self.minimal_altitude
+        self.mode = 'altitude'
         
-    def alt_callback(self, value):
+        # Takeoff action
+        while self.takeoff_pub_.get_num_connections()<1: 
+            print('sending takeoff msg')
+            
+        self.takeoff_pub.publish(self.takeoff_msg)
+        print('takeoff msg recived')
         
-        if cv2.waitKey(1) & 0xFF == ord('g'): self.goal = 1
-        if cv2.waitKey(1) & 0xFF == ord('l'): self.goal = 0.4
+    def alt_callback(self, value:Ardrone3PilotingStateAltitudeChanged):
+        
+        # NumLock affects the unicode of the keyboard, so the "& 0xFF" avoids inconveniences
+        # ord('letter') returns the unicode of the keyboard
+        key = cv2.waitkey(1) & 0xFF
+        
+        if key == ord('g') or key == ord('G'): 
+            self.goal = 1.0
+            
+        if key == ord('l') or key == ord('L'): 
+            self.goal = self.minimal_altitude
+            self.mode = 'landing'
         
         current = value.altitude
         self.control_altitude(current, self.goal)
@@ -29,27 +56,25 @@ class ControlAltitude():
     def control_altitude(self, current, goal):
         vel_msg = Twist()
         
-        # Control de altitud
+        # Altitude control
         Kp = 0.1
         altError = goal - current
-        
-        # Velocidad del dron
         vel_msg.linear.z = Kp*altError
-        
-        # si el goal esta a la altura del land y el valor absoluto del error de velocidad es menor que 0.1 manda land
-        if goal == 0.4 and abs(vel_msg.linear.z) < 0.1: 
-            land_msg = Empty()
-            self.land_pub.publish(land_msg)
-
-            
         self.velocity_pub.publish(vel_msg)
-        print(current)
-        print (vel_msg.linear.z)
+        
+        # landing action
+        if self.mode == 'landing' and abs(altError) < 0.1: 
+            while self.land_pub_.get_num_connections()<1: 
+                print('sending land msg')
+                
+            self.land_pub_.publish(self.land_msg)
+            print('land msg recived')
 
-    
-    
+        
+        print(goal, current, altError, vel_msg.linear.z, self.mode)
+
 def main():
-    rospy.init_node('bebop_plan', anonymous=True)
+    rospy.init_node('bebop_control_altitude', anonymous=True)
     node = ControlAltitude()
     rospy.spin()
     
