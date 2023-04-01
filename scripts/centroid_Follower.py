3b7af33ab78e70d1e51e3aab5e6d2a9cfe9a868a
Xpsp <juliodltv@gmail.com>
2023-03-31 20:14:56 -0600

    take off and land to control altitude

--- scripts/centroid_Follower.py
+++ scripts/centroid_Follower.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
-
+import cv2
 import rospy
 from bebop_msgs.msg import Ardrone3PilotingStateAltitudeChanged
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
 
 class FollowCentroids():
     def __init__(self):
         self.velocity_pub = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size=10)
+        self.takeoff_pub = rospy.Publisher('/bebop/takeoff', Empty, queue_size=10)
+        self.land_pub = rospy.Publisher('/bebop/land', Empty, queue_size=10)
         rospy.Subscriber("/bebop/states/ardrone3/PilotingState/AltitudeChanged", Ardrone3PilotingStateAltitudeChanged, self.alt_callback, queue_size=10)
+        
+        msg = Empty()
+        self.takeoff_pub.publish(msg)
         
     def alt_callback(self, value):
         self.control_altitude(value.altitude)
@@ -17,11 +23,10 @@ class FollowCentroids():
         # Altitud deseada 
         Goal = 1.5
         # Current = alt_msg.altitude
-        Kp = 1
+        Kp = 0.1
 
         # Control de altitud
-        while (current < Goal):
-            
+        if current < Goal:
             altError = Goal - current
             # Velocidad del dron
             vel_msg.linear.z = Kp*altError
@@ -33,6 +38,7 @@ def main():
     rospy.init_node('bebop_plan', anonymous=True)
     node = FollowCentroids()
     rospy.spin()
+    
 
 if __name__ == '__main__':
     try:
--- scripts/control_Altitude.py
+++ scripts/control_Altitude.py
@@ -1,38 +1,62 @@
 #!/usr/bin/env python
 
+import cv2
 import rospy
 from bebop_msgs.msg import Ardrone3PilotingStateAltitudeChanged
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
 
 class ControlAltitude():
     def __init__(self):
         self.velocity_pub = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size=10)
+        self.takeoff_pub = rospy.Publisher('/bebop/takeoff', Empty, queue_size=10)
+        self.land_pub = rospy.Publisher('/bebop/land', Empty, queue_size=10)
         rospy.Subscriber("/bebop/states/ardrone3/PilotingState/AltitudeChanged", Ardrone3PilotingStateAltitudeChanged, self.alt_callback, queue_size=10)
         
+        self.key = None
+        self.goal = 0
+        
+    def take_off(self):
+        # Count para darle tiempo a que llege a su altura takeoff
+        count = 1000
+        msg = Empty()
+        
+        while count>0:
+            self.takeoff_pub.publish(msg)
+            count -= 1
+        
     def alt_callback(self, value):
-        self.control_altitude(value.altitude)
+        
+        if cv2.waitKey(1) & 0xFF == ord('t'): self.take_off()
+        if cv2.waitKey(1) & 0xFF == ord('g'): self.goal = 1
+        if cv2.waitKey(1) & 0xFF == ord('l'): self.goal = 0.2
+        
+        current = value.altitude
+        self.control_altitude(current, self.goal)
     
-    def control_altitude(self, current):
+    def control_altitude(self, current, goal):
         vel_msg = Twist()
-        # Altitud deseada 
-        Goal = 1.5
-        # Current = alt_msg.altitude
-        Kp = 1
-
+        
         # Control de altitud
-        while (current < Goal):
-            
-            altError = Goal - current
-            # Velocidad del dron
-            vel_msg.linear.z = Kp*altError
-            self.velocity_pub.publish(vel_msg)
+        Kp = 0.1
+        altError = goal - current
+        
+        # Velocidad del dron
+        vel_msg.linear.z = Kp*altError
+        
+        land_msg = Empty()
+        if goal == 0.2 and abs(vel_msg.linear.z) < 0.1: self.land_pub.publish(land_msg)
+        self.velocity_pub.publish(vel_msg)
         print(current)
         print (vel_msg.linear.z)
 
+    
+    
 def main():
     rospy.init_node('bebop_plan', anonymous=True)
     node = ControlAltitude()
     rospy.spin()
+    
 
 if __name__ == '__main__':
     try:
